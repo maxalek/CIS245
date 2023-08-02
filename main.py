28f0c1ef7c6ea59a4179ee361def166cc96f750c
maxalek <140433307+maxalek@users.noreply.github.com>
2023-08-02 01:26:30 +0000

    added last_name variable, changed username to first_name

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-username = input('What is your name?\n')
-print(f'Hi, {username}.')
+first_name = input('What is your first name?\n')
+last_name = input("What's your last name?\n")
+print(f'Hi, {first_name} {last_name}.')
+
+
